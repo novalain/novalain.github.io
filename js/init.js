0ba3c0a4a7627196594f5a24243b6c5a1e396ebd
Michael Novén <michaelnoven@206-85.priv23.nus.edu.sg>
2015-11-15 22:44:15 +0800

    init

---
+++ js/init.js
@@ -0,0 +1,32 @@
+(function($){
+
+	$('#index-banner').css('height', $(window).height() - $('.nav-wrapper').height());
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+
+
+
+
+  }); // end of document ready
+
+
+  $(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+
+})(jQuery); // end of jQuery name space
