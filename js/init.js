ea281a8cadeee0ddaa4e6ea055e22518a02cac7e
Michael Novén <michaelnoven@210-217.priv27.nus.edu.sg>
2016-01-12 00:32:25 +0800

    added new content, loading, links etc

--- js/init.js
+++ js/init.js
@@ -1,14 +1,60 @@
+/* Simple JS to control basic functions of the page, jQuery is used due to lazyness */
+
+var windowHeight;
+var bannerHeight = $('#main-nav').height();
+var fadeTextThreshold = 150;
+
+// listen for resize event and change variable
+$(window).resize(function(){
+    windowHeight = $(window).height(); // get new height after change
+    $('#index-banner').height(windowHeight);
+});
+
+// listen for events while scrolling, change color of banner when a certain 
+// threshold is reached
+$(window).scroll(function() {
+	  
+	var scroll = $(window).scrollTop();
+
+	$('.arrow').css({'opacity':( fadeTextThreshold - scroll )/fadeTextThreshold});
+	$('#front-text').css({'opacity':( fadeTextThreshold - scroll )/fadeTextThreshold});
+
+	if(scroll >= windowHeight - bannerHeight){ // Set color
+		$('#main-nav').addClass('colorize-nav');
+		$('#main-nav').removeClass('no-shadow');
+		$('.nav-link').addClass('black-links')
+		$('.brand-logo').addClass('black-links')
+
+	} else { // Set transparent
+		$('#main-nav').removeClass('colorize-nav');
+		$('#main-nav').addClass('no-shadow');
+		$('.brand-logo').removeClass('almost-white-text')
+		$('.nav-link').removeClass('black-links')
+		$('.brand-logo').removeClass('black-links')
+	}
+
+});
+
+
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");;
+});
+
+// document ready
 (function($){
 
-	$('#index-banner').css('height', $(window).height() - $('.nav-wrapper').height());
-  $(function(){
+	windowHeight = $(window).height();
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+	$('#index-banner').css('height', $(window).height());
+	$('.side-nav').css('height', windowHeight);
 
-  }); // end of document ready
+	$(function(){	
+	  $('.button-collapse').sideNav();
+		$('.parallax').parallax();
+	}); // end of document ready
 
-
+  // script for animating the links to the position of content
   $(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -16,7 +62,7 @@
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 	      if (target.length) {
 	        $('html,body').animate({
-	          scrollTop: target.offset().top
+	          scrollTop: target.offset().top - bannerHeight
 	        }, 1000);
 	        return false;
 	      }
